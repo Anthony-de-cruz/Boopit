bf81146528bc4dd89d8189ef0f012275e2e9c7db
TobyTowler <tobytowler11@gmail.com>
2024-04-22 14:23:12 +0100

    working touch sensor

--- Objects/sensortesting.d
+++ Objects/sensortesting.d
@@ -1,3 +1,28 @@
 .\objects\sensortesting.o: SensorTesting.c
-.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.0.0\Board\Board_LED.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal.h
+.\objects\sensortesting.o: C:\Users\Student\Documents\Embedded Systems\CW\Bopit\RTE\Device\STM32F746NGHx\stm32f7xx_hal_conf.h
+.\objects\sensortesting.o: C:\Users\Student\Documents\Embedded Systems\CW\Bopit\RTE\RTE_Components.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_rcc.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_def.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\stm32f7xx.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\stm32f746xx.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm7.h
 .\objects\sensortesting.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmSimd.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\system_stm32f7xx.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\Legacy/stm32_hal_legacy.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_rcc_ex.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_gpio.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_gpio_ex.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_dma.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_dma_ex.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_cortex.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_flash.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_flash_ex.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_pwr.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.1.0\Drivers\STM32F7xx_HAL_Driver\Inc\stm32f7xx_hal_pwr_ex.h
+.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.0.0\Board\Board_LED.h
--- SensorTesting.c
+++ SensorTesting.c
@@ -1,20 +1,44 @@
+#include "stm32f7xx_hal.h"
+#include "stm32f7xx_hal_gpio.h"
 #include "Board_LED.h"                  // Board Support:LED
 
 #define TIME 1000000
 
-int main(void)
-{
-		unsigned int i;
-	
-	  LED_Initialize();                     // LED Init   
-	
-	while(1)	{    		/* Loop forever */
-	
-	  LED_On (0U);                                // Switch LED on
-    for (i = 0; i < TIME; i++)
-			/* empty statement */ ;       /* Wait */
-    LED_Off (0U);                               // Switch off
-    for (i = 0; i < TIME; i++)
-			/* empty statement */ ;       /* Wait */
-	}
+void touchSensor(void);
+
+int main(void){
+    
+    touchSensor();
 }
+
+
+void touchSensor(void){
+    GPIO_InitTypeDef gpio; 
+    
+    	  LED_Initialize(); 
+	
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+ 
+
+	gpio.Mode = GPIO_MODE_INPUT;
+	gpio.Pull = GPIO_PULLDOWN; 
+	gpio.Speed = GPIO_SPEED_HIGH; 
+    
+    gpio.Pin = GPIO_PIN_6;
+	HAL_GPIO_Init(GPIOC, &gpio);
+    
+    while(1){
+        
+       if( HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)  ){
+       
+            LED_On(0U);
+       }
+       
+       else{
+           LED_Off(0U); 
+       }
+    }
+
+
+}
+
