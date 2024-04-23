a2b2a88f27ce97a0ba77ffd6641ad1f8b80ed2f3
Anthony de Cruz <anthonydecruz@proton.me>
2024-04-23 11:22:05 +0100

    removed compiled files

--- Objects/sensortesting.d
+++
@@ -1,3 +0,0 @@
-.\objects\sensortesting.o: SensorTesting.c
-.\objects\sensortesting.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.0.0\Board\Board_LED.h
-.\objects\sensortesting.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
