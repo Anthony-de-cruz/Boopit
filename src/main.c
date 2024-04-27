c0a4c1d08c69a16cc023b3b56c77712b36821fdc
Anthony de Cruz <anthonydecruz@proton.me>
2024-04-27 15:26:54 +0100

    glcd

---
+++ src/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "stm32f7xx_hal.h"
+
+#include "GLCD_Config.h"
+#include "Board_GLCD.h"
+#include "Board_Touch.h"
+#define wait_delay HAL_Delay
+extern GLCD_FONT GLCD_Font_6x8;
+extern GLCD_FONT GLCD_Font_16x24;
+
+
+#ifdef __RTX
+extern uint32_t os_time;
+uint32_t HAL_GetTick(void) {
+	return os_time;
+}
+#endif
+
+/**
+* System Clock Configuration
+*/
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	/* Enable Power Control clock */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	/* The voltage scaling allows optimizing the power
+	consumption when the device is clocked below the
+	maximum system frequency. */
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/* Enable HSE Oscillator and activate PLL
+	with HSE as source */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 25;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 7;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	/* Select PLL as system clock source and configure
+	the HCLK, PCLK1 and PCLK2 clocks dividers */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | 
+	RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+}
