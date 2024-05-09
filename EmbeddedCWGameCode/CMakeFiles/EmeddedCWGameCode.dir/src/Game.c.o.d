7ae13f8b1ddb4e3be2dc0c1d0af3ab29b16ac609
Anthony de Cruz <anthonydecruz@proton.me>
2024-05-09 17:03:19 +0100

    deleted concept code

--- EmbeddedCWGameCode/CMakeFiles/EmeddedCWGameCode.dir/src/Game.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/EmeddedCWGameCode.dir/src/Game.c.o: \
- /home/toby/Programming/C/EmbeddedCWGameCode/src/Game.c \
- /usr/include/stdc-predef.h \
- /home/toby/Programming/C/EmbeddedCWGameCode/src/Game.h
--- EmbeddedCWGameCode/src/main.c
+++
@@ -1,8 +0,0 @@
-#include "Game.h"
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    playGame();
-    return 0;
-}
